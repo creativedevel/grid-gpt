c4fcc5c8fc7c3b9be1f19ccadec76c81ec093078
creativedevel <alfaridhoana@gmail.com>
2018-03-08 11:26:22 +0700

    update gpt

--- gpt-nakita-desktop.js
+++ gpt-nakita-desktop.js
@@ -14,20 +14,41 @@
 
 //<!-- Start: GPT Async -->
     // Load GPT asynchronously
-	var gptadslots=[];
-	var googletag = googletag || {};
-	googletag.cmd = googletag.cmd || [];
-	(function(){ var gads = document.createElement('script');
-		gads.async = true; gads.type = 'text/javascript';
-		var useSSL = 'https:' == document.location.protocol;
-		gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
-		var node = document.getElementsByTagName('script')[1];
-		node.parentNode.insertBefore(gads, node);
-	})();
+	var PWT={}; //Initialize Namespace
+    var gptadslots=[];
+    var googletag = googletag || {};
+      googletag.cmd = googletag.cmd || [];
+      PWT.jsLoaded = function(){ //PubMatic pwt.js on load callback is used to load GPT
+        (function(){ var gads = document.createElement('script');
+            gads.async = true; gads.type = 'text/javascript';
+            var useSSL = 'https:' == document.location.protocol;
+            gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
+            var node = document.getElementsByTagName('script')[0];
+            node.parentNode.insertBefore(gads, node);
+        })();
+      };
 //<!-- Endof: GPT Async -->
 
-// Pubmatic will be here
-// Endof Pubmatic
+//<!-- Pubmatic -->
+(function() {
+      var purl = window.location.href;
+      var url = '//ads.pubmatic.com/AdServer/js/pwt/156479/507';
+      var profileVersionId = '';
+      if(purl.indexOf('pwtv=')>0){
+          var regexp = /pwtv=(.*?)(&|$)/g;
+          var matches = regexp.exec(purl);
+          if(matches.length >= 2 && matches[1].length > 0){
+                          profileVersionId = '/'+matches[1];
+          }
+      }
+      var wtads = document.createElement('script');
+      wtads.async = true;
+      wtads.type = 'text/javascript';
+      wtads.src = url+profileVersionId+'/pwt.js';
+      var node = document.getElementsByTagName('script')[0];
+      node.parentNode.insertBefore(wtads, node);
+})();
+//<!-- Endof Pubmatic -->
     
 	googletag.cmd.push(function() {	
         // Define the ad slots
